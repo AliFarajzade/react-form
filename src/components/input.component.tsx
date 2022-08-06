464686b4bf5760f9f2415103cf81063ad66958d9
Ali Farajzade <alifarajzade1@yahoo.com>
2022-08-06 13:51:27 +0430

    Add error UI.

--- src/components/input.component.tsx
+++ src/components/input.component.tsx
@@ -2,18 +2,31 @@ import { forwardRef } from 'react'
 
 interface IProps {
     className?: string
+    isError?: boolean
+    errorText?: string
     [x: string]: any
 }
 
 const Input = forwardRef<HTMLInputElement, IProps>(
     (
         {
+            isError,
+            errorText,
             className = 'input input-bordered input-primary w-full text-base',
             ...props
         },
         ref
     ) => {
-        return <input {...props} className={className} ref={ref} />
+        return (
+            <>
+                <input {...props} className={className} ref={ref} />
+                {isError && (
+                    <label className="text-sm text-red-500 text-left">
+                        {errorText}
+                    </label>
+                )}
+            </>
+        )
     }
 )
 
