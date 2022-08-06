c3002456dd23240fbae316c377e25c36c91b13ed
Ali Farajzade <alifarajzade1@yahoo.com>
2022-08-06 12:11:13 +0430

    Add routing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
+import Layout from './components/layout.component'
+import Results from './components/results.component'
+import Step1 from './components/step-1.component'
+import Step2 from './components/step-2.component.'
+import Step3 from './components/step-3.component'
+
 const App: React.FC = () => {
-    return null
+    return (
+        <Routes>
+            <Route element={<Layout />} path="/">
+                <Route index element={<Step1 />} />
+                <Route path="step-2" element={<Step2 />} />
+                <Route path="step-3" element={<Step3 />} />
+                <Route path="results" element={<Results />} />
+            </Route>
+        </Routes>
+    )
 }
 
 export default App
