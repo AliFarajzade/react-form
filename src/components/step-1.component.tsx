874f1451ba8fdac1361dd171004b11f377462734
Ali Farajzade <alifarajzade1@yahoo.com>
2022-08-06 13:10:47 +0430

    Add step 1 markup.

--- src/components/step-1.component.tsx
+++ src/components/step-1.component.tsx
@@ -1,5 +1,24 @@
+import Form from './form.component'
+import Input from './input.component'
+
 const Step1: React.FC = () => {
-    return <h2>Step 1</h2>
+    return (
+        <Form>
+            <Input
+                type="text"
+                placeholder="First Name"
+                name="firstName"
+                className="input input-bordered input-primary w-full text-base"
+            />
+            <Input
+                type="text"
+                placeholder="Last Name"
+                name="lastName"
+                className="input input-bordered input-primary w-full text-base"
+            />
+            <button className="btn btn-primary btn-block">Next</button>
+        </Form>
+    )
 }
 
 export default Step1
