45c4994bfd9ac7a7d6dc7f667f4e5907a79acdbd
Ali Farajzade <alifarajzade1@yahoo.com>
2022-08-06 12:10:26 +0430

    Create header component.

---
+++ src/components/header.component.tsx
@@ -0,0 +1,11 @@
+const Header: React.FC = () => {
+    return (
+        <header>
+            <h1 className="text-2xl text-accent text-center">
+                The Ultimate React Form
+            </h1>
+        </header>
+    )
+}
+
+export default Header
