5105a97eeab3deecf5979cc99808c2a1fd2f62f7
Ali Farajzade <alifarajzade1@yahoo.com>
2022-08-07 12:00:25 +0430

    Handle form submission.

--- src/components/step-1.component.tsx
+++ src/components/step-1.component.tsx
@@ -1,6 +1,8 @@
 import { yupResolver } from '@hookform/resolvers/yup'
-import { useForm } from 'react-hook-form'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
 import * as yup from 'yup'
+import { useFormData } from '../contexts/form.context'
 import Form from './form.component'
 import Input from './input.component'
 
@@ -21,17 +23,28 @@ type TInputs = {
 }
 
 const Step1: React.FC = () => {
+    const navigate = useNavigate()
+
     const {
         register,
         formState: { errors },
+        handleSubmit,
     } = useForm<TInputs>({
         mode: 'onBlur',
         resolver: yupResolver(schema),
     })
+    const onSubmit: SubmitHandler<TInputs> = data => {
+        console.log(data)
+        handleChangeFormData(data)
+        navigate('/step-2')
+    }
+
+    const { formData, handleChangeFormData } = useFormData()
 
     return (
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
             <Input
+                defaultValue={formData.firstName}
                 type="text"
                 placeholder="First Name"
                 className="input input-bordered input-primary w-full text-base"
@@ -40,6 +53,7 @@ const Step1: React.FC = () => {
                 errorText={errors.firstName?.message}
             />
             <Input
+                defaultValue={formData.lastName}
                 type="text"
                 placeholder="Last Name"
                 className="input input-bordered input-primary w-full text-base"
@@ -47,9 +61,7 @@ const Step1: React.FC = () => {
                 isError={!!errors.lastName}
                 errorText={errors.lastName?.message}
             />
-            <button type="submit" className="btn btn-primary btn-block">
-                Next
-            </button>
+            <button className="btn btn-primary btn-block">Next</button>
         </Form>
     )
 }
